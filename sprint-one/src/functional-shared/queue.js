fe8dc84a9a133f7244ac236cc457e0f1b0f6488f
chiaS <pretend15@gmail.com>
2014-10-29 16:41:46 -0700

    up to functional-shared completed

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,24 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+  newQueue.storage = {};
+
+  _.extend(newQueue, queueMethods);
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.enque = function(value){
+  this.storage[this.size()] = value;
+};
 
+queueMethods.deque = function(){
+
+};
+
+queueMethods.size = function(){
+  return Object.keys(this.storage).length;
+};
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,25 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.storage = {};
+
+  _.extend(newStack, stackMethods);
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.pop = function(){
+  var value = this.storage[this.size() - 1];
+  delete this.storage[this.size() - 1];
+  return value;
+};
 
+stackMethods.push = function(value){
+  this.storage[this.size()] = value;
+};
+
+stackMethods.size = function(){
+  return Object.keys(this.storage).length;
+};
