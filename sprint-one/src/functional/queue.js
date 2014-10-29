6283286e4d4e67ab2de4f3184f8c10d91a3ad340
chiaS <pretend15@gmail.com>
2014-10-29 15:22:33 -0700

    finished functional q and stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,28 @@
 var makeQueue = function(){
   var someInstance = {};
 
+
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[someInstance.size()] = value;
   };
 
   someInstance.dequeue = function(){
+    var value = storage[0];
+    delete storage[0];
+    for (var key in storage){
+      storage[key - 1] = storage[key];
+    }
+    delete storage[someInstance.size() - 1];
+    return value;
   };
 
   someInstance.size = function(){
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,19 @@ var makeStack = function(){
 
   // Implement the methods below
   someInstance.push = function(value){
+    var index = someInstance.size();
+    storage[index] = value;
   };
 
   someInstance.pop = function(){
+    var index = someInstance.size() - 1;
+    var result = storage[index];
+    delete storage[index];
+    return result;
   };
 
   someInstance.size = function(){
+    return Object.keys(storage).length;
   };
 
   return someInstance;
