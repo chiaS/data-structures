05510a64f48c2071d1e704389dd4ec298f02c40a
chiaS <pretend15@gmail.com>
2014-10-30 20:06:37 -0700

    finished up to set

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = makeNode(value);
+    if (list.head === null && list.tail === null){
+      list['tail'] = newNode;
+      list['head'] = list['tail'];
+    } else {
+      list['tail'].next = newNode;
+      list['tail'] = newNode;
+
+    }
+
   };
 
   list.removeHead = function(){
+    if (list.head !== null && list.tail !== null){
+      var head = list['head'];
+      list['head'] = head.next; //or list['head'].next
+      return head.value;
+    } else {
+      return null;
+    }
   };
 
   list.contains = function(target){
+    for(var key in list){
+      if(list[key].value === target){
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
