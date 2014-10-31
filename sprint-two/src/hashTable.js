877e647cb1d0f4ae4c7b6fe153bcda5b70953c43
chiaS <pretend15@gmail.com>
2014-10-31 12:38:10 -0700

    passing everything but collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,22 +1,29 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
+  this._keyTable = [];
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+ // debugger;
+  this._storage.set(i, v);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  return this._storage.get(i) || null;
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  debugger;
+  this._storage.each(function(value, index, array){
+      if (i === index){
+        array.splice(index, 1);
+      }
+    });
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
