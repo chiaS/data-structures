35bdbb7558f35575d5ff9b2d8b55d03f50a30e44
chiaS <pretend15@gmail.com>
2014-10-31 19:46:33 -0700

    finished hashTable and tree

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,29 +1,48 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
-  this._keyTable = [];
+
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
- // debugger;
-  this._storage.set(i, v);
+  var existingValue = this._storage.get(i);
+  if(existingValue !== undefined){
+    existingValue.push([k, v]);
+    this._storage.set(i, existingValue);
+  }else{
+    this._storage.set(i, [[k, v]]);
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i) || null;
+  if (this._storage.get(i) !== undefined){
+    var result = this._storage.get(i);
+    for(var j = 0; j < result.length; j++){
+    debugger;
+      if (result[j][0] === k){
+        return result[j][1];
+      }
+    }
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  debugger;
-  this._storage.each(function(value, index, array){
-      if (i === index){
-        array.splice(index, 1);
+  this._storage.each(function(bucket, index, storage){
+      if(index === i){
+        for(var j=0; j<bucket.length; j++){
+          if(bucket[j][0] === k){
+            bucket[j].splice(j, 1);
+          }
+        }
       }
     });
 };
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,7 @@
 var makeTree = function(value){
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
   return newTree;
 };
 
@@ -11,10 +11,31 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var tree = makeTree(value);
+  this.children.push(tree);
 };
 
 treeMethods.contains = function(target){
+
+  var found = false;
+
+  var treeSearch = function(node){
+    debugger;
+    if (node.value === target){
+      found = true;
+    } else {
+    for(var i = 0; i < node.children.length; i++){
+        treeSearch(node.children[i]);
+      }
+    }
+  }
+  treeSearch(this);
+  return found;
+  /*if (treeSearch(this)){
+    return true;
+  } else {
+    return false;
+  }*/
 
 };
 
@@ -22,3 +43,4 @@ treeMethods.contains = function(target){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
